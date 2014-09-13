37113d704ebd4bacfa5e7d25f33dfa1aac4daa79
rofrischmann <robin@rofrischmann.de>
2014-09-13 23:08:58 +0200

    #12 Vcard + Developer edition

--- layout/js/GUIController.js
+++ layout/js/GUIController.js
@@ -37,7 +37,9 @@ function loadContact() {
 function loadAnimateMe() {
     window.location.href = "https://bitbucket.org/Grindelwald/animateme.js";
 }
-
+function loadVCard() {
+    window.location.href = "content/vcard/Robin Frischmann.vcf";
+}
 function showScroller() {
     if (document.childNodes[1].clientHeight < document.childNodes[1].scrollHeight) {
         setTimeout('scrollArrow.style.opacity = "0.8"', 500);
